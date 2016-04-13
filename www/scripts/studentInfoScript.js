ce132522150c7b709c8f53eb05319da9e4adacc1
Ala'a Ezziddin <alaa.ezziddin.aae@gmail.com>
2016-04-13 19:31:03 +0300

    - Deleting jQuery Mobile and adding studentInfoScript.

---
+++ www/scripts/studentInfoScript.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    
+    $.getJSON('http://localhost:38133/', function (data) {
+
+            var vals = [];
+            $.each(data, function (key, val) {
+                vals.push(val);
+            } 
+        );
+        document.write(vals[0]);
+        /*
+        for (index = 0, length = vals.length; index < length; index++) {
+            $("#navUL").append($("<li class=\"active\"><a href=\"#\" class=\"courseLink\">" + vals[index] + "</a></li>"));
+        }
+        */
+        }
+    );
+});
