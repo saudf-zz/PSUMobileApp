d97b53312843a3e5eec0e8193448c0f7c2d1aa13
Ala'a Ezziddin <alaa.ezziddin.aae@gmail.com>
2016-04-14 09:20:52 +0300

    - LogIn.js and index.html page to be fixed.

--- www/scripts/logIn.js
+++ www/scripts/logIn.js
@@ -1,9 +1,34 @@
-﻿function init() { document.addEventListener("deviceready", deviceReady, false); delete init; }
+﻿$(function () {
 
-function deviceReady() {
-    $("#loginForm").submit(function () {
+
+    /*function init() { document.addEventListener("deviceready", deviceReady, false); delete init; }
+
+    function deviceReady() {*/
+
+        $("#loginForm").on("click", function (e) {
+            //disable the button so we can't resubmit while we wait
+            $("#submitButton", this).attr("disabled", "disabled");
+            var u = $("#userId", this).val();
+            var p = $("#userPass", this).val();
+            if (u != '' && p != '') {
+                $.post("http://localhost:38133/", { userID: u, userPass: p }, function (res) {
+                    if (res == true) {
+                        $.mobile.changePage("menu.html");
+                    } else {
+                        navigator.notification.alert("Your login failed", function () { });
+                    }
+                    $("#submitButton").removeAttr("disabled");
+                }, "json");
+            }
+            return false;
+        });
+        /*
+        $("#loginForm").submit(function () {
+
+
         var userID = $('input[name="userID"]').val();
         var userPass = $('input[name="userPass"]').val();
+        document.write(userID);
         $.post("http//:localhost:38133/login.php",
         {
             UserID: userID,
@@ -12,5 +37,8 @@ function deviceReady() {
         function (data, status) {
             document.write("Data: " + data + "\nStatus: " + status);
         });
-    });
-}
+
+        });
+        */
+    /*}*/
+});
