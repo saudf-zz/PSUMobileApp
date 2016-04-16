8c0540f60d2692ac9a5bab39c941ba0f7c4b7960
saudf <Saud.al.saud@windowslive.com>
2016-04-16 16:12:05 +0300

    qr code functionality added, needs a little styling to be in the middle and add a back button

---
+++ www/scripts/qr.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="jquery/jquery-2.2.3.min.js" />
+/// <reference path="jquery/jquery.qrcode.js" />
+/// <reference path="jquery/qrcode.js" />
+$(function () {
+    $('#qrcode').qrcode(window.localStorage.getItem('userID'));
+});
