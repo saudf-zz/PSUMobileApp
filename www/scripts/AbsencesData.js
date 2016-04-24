3e42d82bd83b0e3b2873f3cdd19047f8a123fcbf
saudf <Saud.al.saud@windowslive.com>
2016-04-24 11:09:20 +0300

    minor edits + events handling for absences

--- www/scripts/AbsencesData.js
+++ www/scripts/AbsencesData.js
@@ -1,31 +1,49 @@
 ﻿/// <reference path="jquery/jquery-2.2.3.min.js" />
+/// <reference path="cordova.js" />
 
-$(function () {
-
-    var url = "http://localhost:38133/index.php?req=absences";
+(function () {
 
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
         document.addEventListener('pause', onPause.bind(this), false);
         document.addEventListener('resume', onResume.bind(this), false);
-        /*function showHint(str) {
-            xhttp.open("POST", url, true);
-            xhttp.send();
-        }*/
 
-        $.ajax({ 
-            type: "POST",
-            dataType: "JSON",
-            data: { sid: window.localStorage.getItem("sid") },
-            url: url,
-            success: function (data, s, x) {
-                // Ala'a and Khalid: the data returned will be data[rownumber][0] for count of absences and data[rownumber][1] for course name. Plese fill the table as you wish.
-            },
-            fail: function () {
-                alert('error');
+        $(function () {
+
+            var url = "http://localhost:38133/index.php?req=absences";
+
+            document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+            function onDeviceReady() {
+                document.addEventListener('pause', onPause.bind(this), false);
+                document.addEventListener('resume', onResume.bind(this), false);
+                /*function showHint(str) {
+                    xhttp.open("POST", url, true);
+                    xhttp.send();
+                }*/
+
+                $.ajax({
+                    type: "POST",
+                    dataType: "JSON",
+                    data: { sid: window.localStorage.getItem("sid") },
+                    url: url,
+                    success: function (data, s, x) {
+                        // Ala'a and Khalid: the data returned will be data[rownumber][0] for count of absences and data[rownumber][1] for course name. Plese fill the table as you wish.
+                    },
+                    fail: function () {
+                        alert('error');
+                    }
+                });
+
             }
         });
+    };
+    function onPause() {
+        // 
+    };
 
-    }
-});
+    function onResume() {
+        // 
+    };
+})();
