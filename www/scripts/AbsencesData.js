848e3897b7e573a32cd63b8c997aa2fba06a5b4d
Ala'a Ezziddin <alaa.ezziddin.aae@gmail.com>
2016-04-27 19:15:19 +0300

    - Displaying absences on the table.

--- www/scripts/AbsencesData.js
+++ www/scripts/AbsencesData.js
@@ -2,7 +2,7 @@
 /// <reference path="cordova.js" />
 
 (function () {
-
+    
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
@@ -11,17 +11,19 @@
 
         $(function () {
 
-            var url = "http://localhost:38133/index.php?req=absences";
 
             document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+            var url = "http://localhost:38133/index.php?req=absences";
 
             function onDeviceReady() {
                 document.addEventListener('pause', onPause.bind(this), false);
                 document.addEventListener('resume', onResume.bind(this), false);
-                /*function showHint(str) {
+                function showHint(str) {
                     xhttp.open("POST", url, true);
                     xhttp.send();
-                }*/
+                }
+
 
                 $.ajax({
                     type: "POST",
@@ -29,21 +31,24 @@
                     data: { sid: window.localStorage.getItem("sid") },
                     url: url,
                     success: function (data, s, x) {
-                        // Ala'a and Khalid: the data returned will be data[rownumber][0] for count of absences and data[rownumber][1] for course name. Plese fill the table as you wish.
+                        for (var i = data.length - 1; i >= 0 ; i--) {
+                            $("#absencesTable").append("<tr><td>" + data[i][0] + "</td><td>" + data[i][1]
+                                + "</td><td>" + data[i][2] + "</td><td>" + data[i][3] + "</td></tr>");
+                        }
                     },
                     fail: function () {
                         alert('error');
                     }
                 });
 
-            }
+            };
+            function onPause() {
+                // 
+            };
+
+            function onResume() {
+                // 
+            };
         });
     };
-    function onPause() {
-        // 
-    };
-
-    function onResume() {
-        // 
-    };
-})();
+});
