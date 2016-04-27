1014fc6ea6fabe8b4f2cb4f3eb3a04b7232af178
saudf <Saud.al.saud@windowslive.com>
2016-04-27 19:34:07 +0300

    minor edit to absences

--- www/scripts/AbsencesData.js
+++ www/scripts/AbsencesData.js
@@ -2,7 +2,7 @@
 /// <reference path="cordova.js" />
 
 (function () {
-    
+
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
@@ -10,45 +10,31 @@
         document.addEventListener('resume', onResume.bind(this), false);
 
         $(function () {
-
-
-            document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-
             var url = "http://localhost:38133/index.php?req=absences";
 
-            function onDeviceReady() {
-                document.addEventListener('pause', onPause.bind(this), false);
-                document.addEventListener('resume', onResume.bind(this), false);
-                function showHint(str) {
-                    xhttp.open("POST", url, true);
-                    xhttp.send();
-                }
-
-
-                $.ajax({
-                    type: "POST",
-                    dataType: "JSON",
-                    data: { sid: window.localStorage.getItem("sid") },
-                    url: url,
-                    success: function (data, s, x) {
-                        for (var i = data.length - 1; i >= 0 ; i--) {
-                            $("#absencesTable").append("<tr><td>" + data[i][0] + "</td><td>" + data[i][1]
-                                + "</td><td>" + data[i][2] + "</td><td>" + data[i][3] + "</td></tr>");
-                        }
-                    },
-                    fail: function () {
-                        alert('error');
+            $.ajax({
+                type: "POST",
+                dataType: "JSON",
+                data: { sid: window.localStorage.getItem("sid") },
+                url: url,
+                success: function (data, s, x) {
+                    for (var i = data.length - 1; i >= 0 ; i--) {
+                        $("#absencesTable").append("<tr><td>" + data[i][0] + "</td><td>" + data[i][1]
+                            + "</td><td>" + data[i][2] + "</td><td>" + data[i][3] + "</td></tr>");
                     }
-                });
+                },
+                fail: function () {
+                    alert('error');
+                }
+            });
 
-            };
-            function onPause() {
-                // 
-            };
-
-            function onResume() {
-                // 
-            };
         });
+        function onPause() {
+            // 
+        };
+
+        function onResume() {
+            // 
+        };
     };
-});
+})();
